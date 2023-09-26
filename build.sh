5760f136061092a08de72873155306dd37378dcc
Hardy Pottinger <hardy.pottinger@gmail.com>
2023-09-26 16:47:19 -0500

    refactor build.sh to use Docker-Compose instead of Podman

--- build.sh
+++ build.sh
@@ -2,39 +2,40 @@
 
 set -euo pipefail
 
-# Set default values for variables (can override each by setting them in the environment)
+# Set default values for variables (can override each by setting them in the
+# environment)
+export DSPACE_WORKSPPACE="${HOME}/dspace-workspace"
+export BACKEND_SRC="${DSPACE_WORKSPPACE}/dspace/docker-compose-cdl.yml"
+export FRONTEND_SRC="${DSPACE_WORKSPPACE}/dspace-angular/docker/docker-compose-cdl-dist.yml"
+export DSPACE_VER="${DSPACE_VER:-7.6}"
 export AWS_PROFILE="${AWS_PROFILE:-cdl-pad-dev}"
-export FUNC="${FUNC:-pub-dspace-dev}"
 export REGION="${REGION:-us-west-2}"
 export ACCT="${ACCT:-866216109762}"
+#TODO: figure out the proper default image names for these
+export BACKEND_IMAGE="${BACKEND_IMAGE:-backend}"
+export FRONTEND_IMAGE="${FRONTEND_IMAGE:-frontend}"
+export OTHER_IMAGES="${OTHER_IMAGES:-dspace/dspace-solr:${DSPACE_VER:-latest}}" # note that these images will be pushed to ECR, but not built, handy for copying images from DockerHub, etc.
 
 # To defeat all docker caching, pass "--no-cache" on the command line and
 # it'll get passed through on the container build step.
 
-# TODO maybe make sure Docker Desktop is running... but it's not clear to me why
-# you can use `pidof docker` to check
-# # Make sure podman is running
-# if ! (netstat -vanp tcp | egrep 63972 > /dev/null); then
-#     echo "Podman doesn't appear to be running. Maybe do this:"
-#     echo "  podman machine start"
-#     exit 1
-# fi
+echo "===== Building Docker images for DSpace using Docker Compose ====="
+docker-compose -f $BACKEND_SRC -f $FRONTEND_SRC build --pull $BACKEND_IMAGE $FRONTEND_IMAGE $OTHER_IMAGES
+docker tag $BACKEND_IMAGE:latest $ACCT.dkr.ecr.$REGION.amazonaws.com/$BACKEND_IMAGE:latest
+docker tag $FRONTEND_IMAGE:latest $ACCT.dkr.ecr.$REGION.amazonaws.com/$FRONTEND_IMAGE:latest
 
-echo "===== Building Dockerfile for DSpace using Docker Desktop ====="
-# FIXME : I don't think "dspace" is a valid directory in this context
-# we'll need to refactor this to find and use the correct Docker-Compose files
-# we will need to run Docker-Compose to build our DSpace images, not podman
-cd dspace && docker build --pull --build-arg CACHEBUST=$(date -Idate) $* -t $FUNC .
-docker tag $FUNC:latest $ACCT.dkr.ecr.$REGION.amazonaws.com/$FUNC:latest
-
-# FIXME: re-enable this scan after you set up the trivyignore file
-# echo "===== Scanning for vulnerabilities ====="
-# trivy --severity critical,high image --exit-code 1 --quiet --scanners vuln --ignore-unfixed $FUNC:latest
+echo "===== Scanning for vulnerabilities ====="
+trivy --severity critical,high image --exit-code 1 --quiet --scanners vuln --ignore-unfixed $BACKEND_IMAGE:latest $FRONTEND_IMAGE:latest $OTHER_IMAGES
 
 echo "===== Logging in to AWS ECR ====="
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCT.dkr.ecr.$REGION.amazonaws.com
 
-echo "===== Pushing image to ECR ====="
-docker push $ACCT.dkr.ecr.$REGION.amazonaws.com/${FUNC}:latest
+echo "===== Pushing images to ECR ====="
+docker push $ACCT.dkr.ecr.$REGION.amazonaws.com/${BACKEND_IMAGE}:latest
+docker push $ACCT.dkr.ecr.$REGION.amazonaws.com/${FRONTEND_IMAGE}:latest
+
+for image in $OTHER_IMAGES; do
+  docker push $ACCT.dkr.ecr.$REGION.amazonaws.com/${image}:latest
+done
 
 echo "===== Build complete ====="
