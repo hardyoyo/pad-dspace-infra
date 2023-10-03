6603c3bc188e202738da735644590d44a87f09f3
Hardy Pottinger <hardy.pottinger@gmail.com>
2023-10-03 14:26:13 -0500

    Add correct default image names for frontend and backend

--- build.sh
+++ build.sh
@@ -4,38 +4,30 @@ set -euo pipefail
 
 # Set default values for variables (can override each by setting them in the
 # environment)
-export DSPACE_WORKSPPACE="${HOME}/dspace-workspace"
-export BACKEND_SRC="${DSPACE_WORKSPPACE}/dspace/docker-compose-cdl.yml"
-export FRONTEND_SRC="${DSPACE_WORKSPPACE}/dspace-angular/docker/docker-compose-cdl-dist.yml"
-export DSPACE_VER="${DSPACE_VER:-7.6}"
+export DSPACE_VERSION_NUMBER="${DSPACE_VERSION_NUMBER:-7.6}"
+export DSPACE_VER="dspace-${DSPACE_VERSION_NUMBER}"
+export DSPACE_WORKSPACE="${HOME}/dspace-workspace"
+export BACKEND_SRC="${DSPACE_WORKSPACE}/dspace/docker-compose-cdl.yml"
+export FRONTEND_SRC="${DSPACE_WORKSPACE}/dspace-angular/docker/docker-compose-cdl-dist.yml"
 export AWS_PROFILE="${AWS_PROFILE:-cdl-pad-dev}"
 export REGION="${REGION:-us-west-2}"
 export ACCT="${ACCT:-866216109762}"
-#TODO: figure out the proper default image names for these
-export BACKEND_IMAGE="${BACKEND_IMAGE:-backend}"
-export FRONTEND_IMAGE="${FRONTEND_IMAGE:-frontend}"
-export OTHER_IMAGES="${OTHER_IMAGES:-dspace/dspace-solr:${DSPACE_VER:-latest}}" # note that these images will be pushed to ECR, but not built, handy for copying images from DockerHub, etc.
+export BACKEND_IMAGE="${BACKEND_IMAGE:-dspace/dspace}"
+export FRONTEND_IMAGE="${FRONTEND_IMAGE:-cdl/dspace-angular}"
+export BACKEND_IMAGE_TAG="${DSPACE_VER:-latest}"
+export FRONTEND_IMAGE_TAG="cdl-latest-dist}"
+export OTHER_IMAGES="${OTHER_IMAGES:-dspace/dspace-solr:${BACKEND_IMAGE_TAG:-latest}}" # note that these images will be pushed to ECR, but not built, handy for copying images from DockerHub, etc.
 
 echo "===== Building Docker images for DSpace using Docker Compose ====="
-docker-compose -f $BACKEND_SRC -f $FRONTEND_SRC build --pull $BACKEND_IMAGE $FRONTEND_IMAGE $OTHER_IMAGES
-docker tag $BACKEND_IMAGE:latest $ACCT.dkr.ecr.$REGION.amazonaws.com/$BACKEND_IMAGE:latest
-docker tag $FRONTEND_IMAGE:latest $ACCT.dkr.ecr.$REGION.amazonaws.com/$FRONTEND_IMAGE:latest
+docker-compose -f $BACKEND_SRC -f $FRONTEND_SRC build --pull $BACKEND_IMAGE:$BACKEND_IMAGE_TAG $FRONTEND_IMAGE:$FRONTENMD_IMAGE_TAG $OTHER_IMAGES
 
-echo "===== Scanning for vulnerabilities ====="
-trivy --severity critical,high image --exit-code 1 --quiet --scanners vuln --ignore-unfixed $BACKEND_IMAGE:latest $FRONTEND_IMAGE:latest $OTHER_IMAGES
-
-echo "===== Logging in to AWS ECR ====="
-aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCT.dkr.ecr.$REGION.amazonaws.com
-
-#TODO; use specific tags instead of latest
 echo "===== Pushing built images to ECR ====="
-docker push $ACCT.dkr.ecr.$REGION.amazonaws.com/${BACKEND_IMAGE}:latest
-docker push $ACCT.dkr.ecr.$REGION.amazonaws.com/${FRONTEND_IMAGE}:latest
+docker push $ACCT.dkr.ecr.$REGION.amazonaws.com/${BACKEND_IMAGE}:${FRONTEND_IMAGE_TAG}
+docker push $ACCT.dkr.ecr.$REGION.amazonaws.com/${FRONTEND_IMAGE}:${BACKEND_IMAGE_TAG}
 
-#TODO: use specific tags instead of latest
-echo "===== Pushing other images to ECR ====="
 for image in $OTHER_IMAGES; do
-  docker push $ACCT.dkr.ecr.$REGION.amazonaws.com/${image}:latest
+    docker push $ACCT.dkr.ecr.$REGION.amazonaws.com/${image}:${BACKEND_IMAGE_TAG}
 done
 
 echo "===== Build complete ====="
+exit 0
