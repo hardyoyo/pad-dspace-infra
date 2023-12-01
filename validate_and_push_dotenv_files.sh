e130c6e1d037b898e2a204701ea6495138aef669
Hardy Pottinger <hardy.pottinger@gmail.com>
2023-12-01 16:34:27 -0600

    WIP, fixing env file and the validate and push script

--- validate_and_push_dotenv_files.sh
+++ validate_and_push_dotenv_files.sh
@@ -2,7 +2,7 @@
 set -eou pipefail
 
 # Set AWS profile and function name
-export AWS_PROFILE=cdl-pad-dev
+export AWS_PROFILE=${AWS_PROFILE:-cdl-pad-dev}
 export FUNC=pub-dspace-dev
 
 # Specify the S3 bucket
@@ -24,6 +24,6 @@ dotenv-linter -rq
 
 echo "Pushing all dotenv files to S3..."
 # Copy dotenv file to S3 bucket
-aws s3 cp $DOTENV_FILES "s3://$S3_BUCKET/$S3_FOLDER"
+aws s3 cp $DOTENV_FILES "s3://$S3_BUCKET/$S3_FOLDER/"
 
 exit 0
