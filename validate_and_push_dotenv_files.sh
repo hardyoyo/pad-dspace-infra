4109f7e49c4aaf5e361ad3eaa0503f02ecc0fcd9
Hardy Pottinger <hardy.pottinger@gmail.com>
2023-12-05 16:09:42 -0600

    Fix-up shellcheck concern, adjust reviewdog workflow

--- validate_and_push_dotenv_files.sh
+++ validate_and_push_dotenv_files.sh
@@ -24,6 +24,6 @@ dotenv-linter -rq
 
 echo "Pushing all dotenv files to S3..."
 # Copy dotenv file to S3 bucket
-aws s3 cp $DOTENV_FILES "s3://$S3_BUCKET/$S3_FOLDER/"
+aws s3 cp "$DOTENV_FILES" "s3://$S3_BUCKET/$S3_FOLDER/"
 
 exit 0
